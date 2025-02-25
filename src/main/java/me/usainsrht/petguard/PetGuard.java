7a69cfc657bf5ac89e190a37848c40faf4d39eba
UsainSrht <usainsrht@gmail.com>
2025-02-25 20:42:22 +0300

    upload files

--- src/main/java/me/usainsrht/petguard/PetGuard.java
+++ src/main/java/me/usainsrht/petguard/PetGuard.java
@@ -1,17 +1,32 @@
 package me.usainsrht.petguard;
 
+import org.bukkit.entity.Tameable;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class PetGuard extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        getServer().getPluginManager().registerEvents(new Listener() {
+            @EventHandler
+            public void on(EntityDamageByEntityEvent e) {
+                if (e.getEntity() instanceof Tameable) {
+                    Tameable tameable = (Tameable) e.getEntity();
+                    if (!tameable.isTamed()) return;
+                    if (tameable.getOwner().getUniqueId().equals(e.getDamager().getUniqueId())) {
+                        e.setCancelled(true);
+                        //add a message maybe?
+                    }
+                }
+            }
+        }, this);
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
