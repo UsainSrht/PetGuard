22c4c1823e445e689d8dbbd45bc37da1b74379fd
UsainSrht <usainsrht@gmail.com>
2025-02-25 20:41:51 +0300

    Initial commit

---
+++ src/main/java/me/usainsrht/petguard/PetGuard.java
@@ -0,0 +1,17 @@
+package me.usainsrht.petguard;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PetGuard extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
